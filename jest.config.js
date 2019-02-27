de6f10b7728f4776579c0f152d5fa91e672cb9fa
Ka-wai Wong <Kawai.Wong@Microsoft.com>
2019-02-26 16:41:23 -0800

    Update jest config to collect coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'client/**/*.ts',
+    'server/**/*.ts'
+  ],
+  globals: {
+    'ts-jest': {
+      diagnostics: false
+    }
+  },
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: [ 'node_modules' ]
